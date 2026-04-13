6a9fbb3e08f7fa2f10d87f56fa0184693a04d9aa
parman <tegesitt@gmail.com>
2026-04-14 02:07:21 +0700

    feat: module for DI

---
+++ tauth/lib/src/t_auth_module.dart
@@ -0,0 +1,46 @@
+// file: lib/src/t_auth_module.dart (Di dalam package TAuth)
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'data/datasources/auth_local_data_source.dart';
+import 'data/datasources/auth_remote_datasource.dart';
+import 'data/repositories/auth_repository_impl.dart';
+import 'domain/repositories/i_auth_repository.dart';
+import 'domain/usecases/login_with_email.dart';
+import 'domain/usecases/login_with_google.dart';
+import 'domain/usecases/logout.dart';
+import 'domain/usecases/observe_auth_state.dart';
+
+/// Kelas Factory ini adalah SATU-SATUNYA tempat di mana
+/// aplikasi luar bisa meminta instance dari TAuth.
+class TAuthModule {
+
+  /// Membuat instance AuthRepository yang sudah dirakit lengkap
+  static AuthRepository createRepository({
+    required FirebaseAuth firebaseAuth,
+    required GoogleSignIn googleSignIn,
+    required FlutterSecureStorage secureStorage,
+  }) {
+    // Rakit pekerja lokal
+    final localDataSource = AuthLocalDataSourceImpl(secureStorage);
+
+    // Rakit pekerja internet
+    final remoteDataSource = AuthRemoteDataSourceImpl(
+      firebaseAuth: firebaseAuth,
+      googleSignIn: googleSignIn,
+    );
+
+    // Kembalikan manajernya (Repository)
+    return AuthRepositoryImpl(
+      remoteDataSource: remoteDataSource,
+      localDataSource: localDataSource,
+    );
+  }
+
+  // (Opsional) Factory untuk UseCases jika ingin lebih rapi
+  static ObserveAuthStateUseCase createObserveUseCase(AuthRepository repo) => ObserveAuthStateUseCase(repo);
+  static LoginWithGoogleUseCase createLoginGoogleUseCase(AuthRepository repo) => LoginWithGoogleUseCase(repo);
+  static LoginWithEmailUseCase createLoginEmailUseCase(AuthRepository repo) => LoginWithEmailUseCase(repo);
+  static LogoutUseCase createLogoutUseCase(AuthRepository repo) => LogoutUseCase(repo);
+}
