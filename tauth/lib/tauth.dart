c76de06449498b2e7a64ff789f081b851ac288bd
parman <tegesitt@gmail.com>
2026-04-14 02:07:50 +0700

    feat: module

--- tauth/lib/tauth.dart
+++ tauth/lib/tauth.dart
@@ -1,5 +1,13 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// file: lib/t_auth.dart
+library t_auth;
+
+// Export Core
+export 'src/core/error/failures.dart';
+export 'src/core/usecases/usecase.dart';
+
+// Export Domain
+export 'src/domain/entities/auth_user.dart';
+export 'src/domain/usecases/login_with_email.dart';
+export 'src/domain/usecases/login_with_google.dart'; // <-- EXPORT BARU
+export 'src/domain/usecases/logout.dart';
+export 'src/domain/usecases/observe_auth_state.dart';
