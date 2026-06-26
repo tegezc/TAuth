f02b384e847e2b2b5c908a16eb250bf1de6388a0
parman <tegesitt@gmail.com>
2026-06-26 23:11:09 +0700

    enable firebase

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 // file: example/lib/main.dart
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
 import 'injection.dart';
 import 'blocs.dart';
 
@@ -8,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 1. Inisialisasi Firebase (PENTING: Gunakan file firebase_options.dart Anda)
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // 2. Setup Dependency Injection
   setupDependencies();
